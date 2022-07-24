ab4f3a9f39f15571046710456694f221c595afb0
Lyshka <53864647+Lyshka@users.noreply.github.com>
2022-07-25 02:18:21 +0300

    fix deploy
    fix deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {
 			margin: {
